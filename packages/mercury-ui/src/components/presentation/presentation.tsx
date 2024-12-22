7d31c093b40752bcf231d8cd6ccaadbd1779dab7
rai <96561881+r4ai@users.noreply.github.com>
2024-12-22 17:26:55 +0900

    fix: support custom components

--- packages/mercury-ui/src/components/presentation/presentation.tsx
+++ packages/mercury-ui/src/components/presentation/presentation.tsx
@@ -1,7 +1,7 @@
-import type { MDXContent } from "mdx/types"
+import type { MDXComponents, MDXContent } from "mdx/types"
 import type { FC } from "react"
 import { Redirect, Route, Switch } from "wouter"
-import { components } from "../components"
+import { components as defaultComponents } from "../components"
 import { ControlMenu } from "../control-menu"
 import { Slide } from "../slide"
 import { AllSlidesPresentation } from "./all-slides-presentation"
@@ -9,14 +9,17 @@ import { AllSlidesPresentation } from "./all-slides-presentation"
 export type PresentationProps = {
   base?: string
   slidesLength: number
+  components?: MDXComponents | undefined
   Content: MDXContent
 }
 
 export const Presentation: FC<PresentationProps> = ({
   base = "/",
   slidesLength,
+  components,
   Content,
 }) => {
+  console.log(components)
   return (
     <Route path={base} nest>
       <div className="h-full">
@@ -27,13 +30,14 @@ export const Presentation: FC<PresentationProps> = ({
           <Route path="/all">
             <Content
               components={{
-                ...components,
+                ...defaultComponents,
                 Slide: (props) => <Slide {...props} route={false} />,
                 Presentation: AllSlidesPresentation,
+                ...components,
               }}
             />
           </Route>
-          <Content components={components} />
+          <Content components={{ ...defaultComponents, ...components }} />
         </Switch>
         <ControlMenu
           data-control-menu
