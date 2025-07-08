b05ffcfcf1590dce7d051831e601b667eccece90
rai <96561881+r4ai@users.noreply.github.com>
2025-07-08 23:40:01 +0900

    chore: create new example

---
+++ examples/program/src/main.tsx
@@ -0,0 +1,16 @@
+import { PresentationsProvider } from "@r4ai/mercury-ui";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Presentation from "./Presentation.mdx";
+import "katex/dist/katex.min.css";
+import "./main.css";
+import "@r4ai/mercury-ui/style.css";
+
+// biome-ignore lint/style/noNonNullAssertion: #root is always present in the DOM
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <PresentationsProvider>
+      <Presentation />
+    </PresentationsProvider>
+  </React.StrictMode>,
+);
