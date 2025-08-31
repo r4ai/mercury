902ad42806ff0986433e304eb734b45b11eb00e3
rai <96561881+r4ai@users.noreply.github.com>
2025-08-31 18:06:05 +0900

    refactor(mercury-ui): update component types for better type safety

--- packages/mercury-ui/src/components/components.tsx
+++ packages/mercury-ui/src/components/components.tsx
@@ -1,4 +1,4 @@
-import type { MDXComponents } from "mdx/types"
+import type { FC, ReactNode } from "react"
 import { Blockquote } from "./blockquote"
 import { CodeBlock } from "./code-block"
 import { Heading1, Heading2, Heading3, Heading4 } from "./heading"
@@ -9,21 +9,36 @@ import { List, ListItem, OrderedList } from "./list"
 import { Paragraph } from "./paragraph"
 import { Slide } from "./slide"
 
-// todo: Provide proper types for components instead of MDXComponents
-export const components: MDXComponents = {
+export type Components = {
+  h1: typeof Heading1
+  h2: typeof Heading2
+  h3: typeof Heading3
+  h4: typeof Heading4
+  blockquote: typeof Blockquote
+  a: typeof Link
+  ul: typeof List
+  ol: typeof OrderedList
+  li: typeof ListItem
+  p: typeof Paragraph
+  "inline-code": typeof InlineCode
+  pre: typeof CodeBlock
+  kbd: typeof Keyboard
+  Slide: typeof Slide
+  Presentation: FC<{ children?: ReactNode }>
+}
+
+export const components: Components = {
   h1: Heading1,
   h2: Heading2,
   h3: Heading3,
   h4: Heading4,
   blockquote: Blockquote,
-  // @ts-expect-error
   a: Link,
   ul: List,
   ol: OrderedList,
   li: ListItem,
   p: Paragraph,
   "inline-code": InlineCode,
-  // @ts-expect-error
   pre: CodeBlock,
   kbd: Keyboard,
   Slide,
