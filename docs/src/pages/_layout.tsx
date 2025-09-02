0651a32b5a9706ea88b46c9f8fd9f52de881bd13
rai <96561881+r4ai@users.noreply.github.com>
2025-09-03 00:37:03 +0900

    feat: remove unused files

--- docs/src/pages/_layout.tsx
+++ docs/src/pages/_layout.tsx
@@ -2,37 +2,14 @@ import "../styles/globals.css"
 
 import type { ReactNode } from "react"
 import { Providers } from "@/components/providers"
-import { Footer } from "../components/footer"
-import { Header } from "../components/header"
 
 type RootLayoutProps = { children: ReactNode }
 
-export default async function RootLayout({ children }: RootLayoutProps) {
-  const data = await getData()
-
-  return (
-    <Providers>
-      <div className="font-['Nunito']">
-        <meta name="description" content={data.description} />
-        <link rel="icon" type="image/png" href={data.icon} />
-        <Header />
-        <main className="m-6 flex items-center *:min-h-64 *:min-w-64 lg:m-0 lg:min-h-svh lg:justify-center">
-          {children}
-        </main>
-        <Footer />
-      </div>
-    </Providers>
-  )
+const RootLayout = ({ children }: RootLayoutProps) => {
+  return <Providers>{children}</Providers>
 }
 
-const getData = async () => {
-  const data = {
-    description: "An internet website!",
-    icon: "/images/favicon.png",
-  }
-
-  return data
-}
+export default RootLayout
 
 export const getConfig = async () => {
   return {
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,7 +1,3 @@
-import { Link } from "waku"
-
-import { Counter } from "../components/counter"
-
 export default async function HomePage() {
   const data = await getData()
 
@@ -9,11 +5,6 @@ export default async function HomePage() {
     <div>
       <title>{data.title}</title>
       <h1 className="text-4xl font-bold tracking-tight">{data.headline}</h1>
-      <p>{data.body}</p>
-      <Counter />
-      <Link to="/about" className="mt-4 inline-block underline">
-        About page
-      </Link>
     </div>
   )
 }
