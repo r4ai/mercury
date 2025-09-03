6316ae30733d417f02df8a37c7f1b7581c7768e4
rai <96561881+r4ai@users.noreply.github.com>
2025-09-03 17:31:30 +0900

    refactor(vite-plugin-mercury): simplify inline code test and remove unused code

--- packages/vite-plugin-mercury/src/plugins/mdx/remark-plugins/remark-inline-code.test.ts
+++ packages/vite-plugin-mercury/src/plugins/mdx/remark-plugins/remark-inline-code.test.ts
@@ -1,12 +1,11 @@
 import { describe, expect, it } from "vitest"
 import { createDOMFromHTML, renderHtml } from "../../../test-utils.js"
-import { createMdxPlugins, mercuryMdxDefaultOptions } from "../plugin.js"
 import { remarkInlineCode } from "./remark-inline-code.js"
 
 describe("remark-inline-code", () => {
   it("transforms inline code to <inline-code> when used alone", async () => {
     const mdx = "Hello `world`!"
-    const html = await renderHtml(mdx, [[remarkInlineCode]], [])
+    const html = await renderHtml(mdx, [remarkInlineCode], [])
     const container = createDOMFromHTML(html)
 
     const inlineCodeElement = container.querySelector("inline-code")
@@ -15,18 +14,5 @@ describe("remark-inline-code", () => {
 
     const codeElement = container.querySelector("code")
     expect(codeElement).toBeFalsy()
-  })
-
-  it("works with default mercury MDX plugins", async () => {
-    const input = "Hello `world`!" // simple doc; remark-mercury will wrap as a single slide
-    const { remarkPlugins, rehypePlugins } = createMdxPlugins(
-      mercuryMdxDefaultOptions,
-    )
-    const html = await renderHtml(input, remarkPlugins, rehypePlugins)
-    const container = createDOMFromHTML(html)
-
-    const inlineCodeElement = container.querySelector("inline-code")
-    expect(inlineCodeElement).toBeTruthy()
-    expect(inlineCodeElement?.textContent).toBe("world")
   })
 })
